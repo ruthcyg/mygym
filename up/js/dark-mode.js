2f32185bfe7c5a69d2ba804b10c1a30f95257a4c
Ruthcy G <ewho.ruth2014@gmail.com>
2022-04-30 15:54:21 +0530

    add showcase motto features

--- up/js/dark-mode.js
+++ up/js/dark-mode.js
@@ -1,4 +1,6 @@
 let checkbox = document.querySelector('input[name="theme"]');
+
+
 let htmlElement = document.documentElement;
 ////////////////////////////////////////////////////
 checkbox.addEventListener("change", () => {
